46ef41c54cce5b3baad966fdfb7780eeb36ce4eb
Igor Yampolskiy <clubber_77@mail.ru>
2022-11-13 19:17:22 +0300

    finished task_4

--- Seminar_001/task_4/Program.cs
+++ Seminar_001/task_4/Program.cs
@@ -3,3 +3,24 @@
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
+Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+if ( numberB > numberA & numberB > numberC) 
+{
+Console.WriteLine($"max = {numberB} ");
+}
+else if ( numberC > numberA & numberC > numberB)
+{
+Console.WriteLine($"max = {numberC} ");
+}
+else
+{
+ Console.WriteLine($"max = {numberA} ");   
+}
