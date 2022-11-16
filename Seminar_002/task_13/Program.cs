c9ee9d83297bbe94874be953883f00d7398db778
Igor Yampolskiy <clubber_77@mail.ru>
2022-11-17 00:58:00 +0300

    finished homework 2

---
+++ Seminar_002/task_13/Program.cs
@@ -0,0 +1,63 @@
+﻿
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Clear();
+
+Console.WriteLine("Числовой метод".ToUpper());
+Console.WriteLine();
+
+Console.Write("Введите число: ");
+int num = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+int numCount = num, thirdNum, index = 0, div = 1;
+
+
+if (num < 100)
+    {
+        Console.WriteLine("ВНИМАНИЕ!!! Число двузначное, третьей цифры нет!");
+    }
+else
+    {
+    while ( numCount != 0)
+        {
+            numCount = numCount/10;
+            index++;
+    
+        }
+
+    for (int i=0; i<index-3; i++)
+        {
+            div = div*10;
+        }
+    thirdNum = (num/div)%10;
+    Console.WriteLine($"Третья цифра в числе: {thirdNum}");
+    }
+
+
+Console.WriteLine();
+Console.WriteLine("#######################################################");
+Console.WriteLine();
+
+
+Console.WriteLine("Решение через строковые переменные".ToUpper());
+Console.WriteLine();
+
+Console.Write("Введите число: ");
+string inputStrings = Console.ReadLine();
+int stringLenght = inputStrings.Length;
+
+if (stringLenght < 3)
+    {
+        Console.WriteLine("ВНИМАНИЕ!!! Число двузначное, третьей цифры нет!");
+    }
+else if (inputStrings[0] == '-')
+    {
+        Console.WriteLine($"Третья цифра в числе: {inputStrings[3]}");
+    }
+else
+    {
+        Console.WriteLine($"Третья цифра в числе: {inputStrings[2]}");
+    }
+
---
+++ Seminar_002/task_15/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Clear();
+Console.Write("Введите номер дня недели: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+switch (num)
+{
+    case 1:
+        Console.WriteLine("Будний день.");
+        break;
+    case 2:
+        Console.WriteLine("Будний день.");
+        break;
+    case 3:
+        Console.WriteLine("Будний день.");
+        break;
+    case 4:
+        Console.WriteLine("Будний день.");
+        break;
+    case 5:
+        Console.WriteLine("Будний день.");
+        break;
+    case 6:
+        Console.WriteLine("Выходной.");
+        break;
+    case 7:
+        Console.WriteLine("Выходной");
+        break;
+    default:
+        Console.WriteLine("Дней в неделе не может быть меньше одного и больше семи!");
+        break;
+}
