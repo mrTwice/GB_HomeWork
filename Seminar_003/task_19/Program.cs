748849177994b3b7be12452cb16669bd4d6e9b6a
Igor Yampolskiy <clubber_77@mail.ru>
2022-11-20 23:01:04 +0300

    finished homework after seminar3

---
+++ Seminar_003/task_19/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Clear();
+Console.Write("Введите пятизначное число число: ");
+int number = Math.Abs(Convert.ToInt32(Console.ReadLine())),
+             numCount = number, index = 0, remainders;
+bool palindrome = true;
+
+Console.WriteLine("");
+
+if (number <10000 && number > 99999) // Проверка является ли число пятизначным.
+{
+    Console.Write("Введите пятизначное число число!");
+}
+else
+{
+    while ( numCount != 0) // Подсчет количества цифр в числе.
+        {
+            numCount = numCount/10;
+            index++;
+        }
+
+    int[] numsArray = new int[index]; // Объявление массива, в который будет записано наше число.
+    
+    for (int i = 0; i < index; i++ ) // Запись значений в массив.
+        {
+            remainders = number%10;
+            numsArray[(index-1)-i] = remainders;        
+            number = number/10;
+        }
+    
+    for (int i = 0; i < numsArray.Length/2; i++) // Проверка на палиндромность.
+    {
+        if (numsArray[i] != numsArray[(numsArray.Length-1) - i])
+            {
+                palindrome = false;
+            }
+    }
+
+    if (palindrome == true) // Вывод результата в консоль.
+        {
+            Console.WriteLine("Введенное число - палиндром.");
+        }
+    else 
+        {
+            Console.WriteLine("Введенное число палиндромом не является.");
+        }
+    
+
+}
---
+++ Seminar_003/task_21/Program.cs
@@ -0,0 +1,34 @@
+﻿
+// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+// AB = √(xb - xa)2 + (yb - ya)2 + (zb - za)2
+// Math.Sqrt(object)
+// Math.Pow(args,2)
+// Math.Round(a, 2)
+
+
+
+Console.WriteLine("Введите координаты первой точки: ");
+Console.Write("xA = ");
+double xA = Convert.ToDouble(Console.ReadLine());
+Console.Write("yA = ");
+double yA = Convert.ToDouble(Console.ReadLine());
+Console.Write("zA = ");
+double zA = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите координаты второй точки: ");
+Console.Write("xB = ");
+double xB = Convert.ToDouble(Console.ReadLine());
+Console.Write("yB = ");
+double yB = Convert.ToDouble(Console.ReadLine());
+Console.Write("zB = ");
+double zB = Convert.ToDouble(Console.ReadLine());
+
+double lenghtLine = Math.Round(Math.Sqrt(Math.Pow(xB - xA, 2) + Math.Pow(yB - yA, 2) + Math.Pow(zB - zA, 2)), 2);
+
+
+Console.WriteLine($"Расстояние между точками = {lenghtLine}");
