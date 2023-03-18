091119b5cb7336bfbd15f6359d3307b2a54619f6
Igor Yampolskiy <clubber_77@mail.ru>
2023-03-18 23:26:29 +0300

    add check for null in cell

--- Java/seminar_004/src/HomeWork_004.java
+++ Java/seminar_004/src/HomeWork_004.java
@@ -32,8 +32,10 @@ public class HomeWork_004 {
                 System.out.print("Ввод не корректен! Введите команду или строку: ");
             } else if(afterSplit[0].equals("exit")){
                 break;
+            } else if (afterSplit[0].equals("menu")) {
+                printMenu();
             } else if (afterSplit[0].equals("print")) {
-                if (checkArrayList(data, Integer.parseInt(afterSplit[1]))) {
+                if (checkArrayList(data, Integer.parseInt(afterSplit[1])) && data.get(Integer.parseInt(afterSplit[1])) != null) {
                     System.out.println(String.format("Значение в ячейке: %s", data.get(Integer.parseInt(afterSplit[1]))));
                 } else {
                     System.out.println("Запись не существует!");
@@ -55,6 +57,7 @@ public class HomeWork_004 {
         System.out.println("\t1. Если вы хотите добавить информацию, введите строку и число, разделенные тильдой. Пример: Дом~1)");
         System.out.println("\t2. Если хотите выйти, введите exit");
         System.out.println("\t3. Если хотите посмотреть значение в ячеке, ведите print и номер ячейки через тильду. Пример: print~3");
+        System.out.println("\t4. Хотите посмотреть это меню, введите menu.");
     }
 
     static void extArrayList(ArrayList<String> list, int index){
