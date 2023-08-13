1c15159e63dcdb93c7872bf2dc090f9b043bf656
Igor Yampolskiy <clubber_77@mail.ru>
2023-08-13 11:08:36 +0300

    homework_01 Architecture some changes

--- Architecture/homework_01/src/ModelElements/Poligon.java
+++ Architecture/homework_01/src/ModelElements/Poligon.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Poligon {
-    public List<Point3D> points = new ArrayList<>();
+    public List<Point3D> points;
 
     public Poligon(List<Point3D> points) {
         this.points = points;
