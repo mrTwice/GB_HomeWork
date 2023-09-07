72113cce018053151054048917139a56e14ea778
Igor Yampolskiy <clubber_77@mail.ru>
2023-09-07 23:48:00 +0300

    add Customer class

---
+++ Architecture/homework_07/src/Application/Models/Primitives/Users/Customer.java
@@ -0,0 +1,13 @@
+package Application.Models.Primitives.Users;
+
+public class Customer extends User{
+    private static int idCustomer = 0;
+    public Customer(String firstName, String lastname, String phoneNumber, String emailAddress) {
+        super(firstName, lastname, phoneNumber, emailAddress);
+        this.idCustomer++;
+    }
+
+    public static int getIdCustomer() {
+        return idCustomer;
+    }
+}
