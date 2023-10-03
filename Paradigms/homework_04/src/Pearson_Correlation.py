a4959c0d1f511841ad27c554106d2d1b852150c1
Igor Yampolskiy <clubber_77@mail.ru>
2023-10-03 23:56:01 +0300

    some visual fixes

--- Paradigms/homework_04/src/Pearson_Correlation.py
+++ Paradigms/homework_04/src/Pearson_Correlation.py
@@ -4,9 +4,14 @@ from statistics import mean
 def pearson_correlation(array_x, array_y):
     x_mean = mean(array_x)
     y_mean = mean(array_y)
+    return (
+                sum(list(map(lambda x,y: (x - x_mean) * (y - y_mean), array_x, array_y)))/
+                (
+                    math.sqrt(sum(list(map(lambda x: (x - x_mean)**2, array_x)))) *
+                    math.sqrt(sum(list(map(lambda y: (y - y_mean)**2, array_y))))
+                )
+            )
 
-    return (sum(list(map(lambda x,y: (x - x_mean) * (y - y_mean), array_x, array_y)))/
-            (math.sqrt(sum(list(map(lambda x: (x - x_mean)**2, array_x)))) * math.sqrt(sum(list(map(lambda y: (y - y_mean)**2, array_y))))))
 array_x = [1,2,4,5,6]
 array_y = [2,4,8,10,12]
 
