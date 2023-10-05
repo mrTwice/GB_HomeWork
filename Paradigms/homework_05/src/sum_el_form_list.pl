29ac64dca009fda28c22d1e57e65aa84e5c6226b
Igor Yampolskiy <clubber_77@mail.ru>
2023-10-05 21:51:17 +0300

    homework_05

---
+++ Paradigms/homework_05/src/sum_el_form_list.pl
@@ -0,0 +1,8 @@
+% Сумма элементов списка
+summ_el_from_list([], 0).
+summ_el_from_list([Head|Tail], Sum) :-
+% Вызов рекурсивной функции
+    summ_list(Tail, SumTmp), Sum is SumTmp + Head.
+
+% Запрос
+?- summ_el_from_list([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], Sum)
