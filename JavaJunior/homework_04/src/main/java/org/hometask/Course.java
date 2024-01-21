e2782375bbaeb2fceb744879c2347f89abba5c51
Igor Yampolskiy <clubber_77@mail.ru>
2024-01-21 16:49:57 +0300

    Homework_04 Created docker-compose.yml file with .env file for database settings. Add maven dependencies for Hibernate, MySQL. Setup Course.java

---
+++ JavaJunior/homework_04/src/main/java/org/hometask/Course.java
@@ -0,0 +1,54 @@
+package org.hometask;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Courses")
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name="Title")
+    private String title;
+    @Column(name="Duration")
+    private String duration;
+
+    public Course() {
+    }
+
+    public Course(String title, String duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+
+    public Course(int id, String title, String duration) {
+        this.id = id;
+        this.title = title;
+        this.duration = duration;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + id + "] " + title + " " + duration;
+    }
+}
